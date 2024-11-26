b1860af86da3235beb1f33af5ca97088f975a678
CarSerr <matachangos777@gmail.com>
2024-11-26 07:24:45 -0600

    terraform

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "TIENDA_TOKEN" {
+  type = string
+}
+
+variable "DB_NAME" {
+  type = string
+}
+ 
+variable "MYSQL_HOST" {
+type= string
+}
+ 
+variable "DB_PASSWORD" {
+  type = string
+}
+ 
+variable "DB_USER" {
+  type = string
+}
+
+variable "DOMAIN" {
+  type = string
+}
+
+variable "USER_EMAIL" {
+  type = string
+}
+
+variable "DB_CLUSTER" {
+  type = string
+}
